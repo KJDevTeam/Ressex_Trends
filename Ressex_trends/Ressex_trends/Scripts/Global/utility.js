bc29417c85f313ee2e9293242c9ef4a04d6383b2
aritra-001 <aritraiiit@gmail.com>
2023-04-14 19:26:56 +0530

    update
    update

--- Ressex_trends/Ressex_trends/Scripts/Global/utility.js
+++ Ressex_trends/Ressex_trends/Scripts/Global/utility.js
@@ -343,49 +343,49 @@ utility.logoffuserLogin= function () {
 
 /////////////////////////////JSON TO CSV///////////////////////////////
 
-//utility.convertToCSV = function (objArray) {
-//    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-//    var str = '';
+utility.convertToCSV = function (objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
 
-//    for (var i = 0; i < array.length; i++) {
-//        var line = '';
-//        for (var index in array[i]) {
-//            if (line != '') line += ','
-//            line += array[i][index];
-//        }
-//        str += line + '\r\n';
-//    }
-//    return str;
-//}
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+            line += array[i][index];
+        }
+        str += line + '\r\n';
+    }
+    return str;
+}
 
-//utility.exportCSVFile = function (headers, items, fileTitle) {
-//    if (headers) {
-//        items.unshift(headers);
-//    }
+utility.exportCSVFile = function (headers, items, fileTitle) {
+    if (headers) {
+        items.unshift(headers);
+    }
 
-//    // Convert Object to JSON
-//    var jsonObject = JSON.stringify(items);
+    // Convert Object to JSON
+    var jsonObject = JSON.stringify(items);
 
-//    var csv = utility.convertToCSV(jsonObject);
-//    var exportedFilenmae = fileTitle + '.csv';  //|| 'export.csv';
+    var csv = utility.convertToCSV(jsonObject);
+    var exportedFilenmae = fileTitle + '.csv';  //|| 'export.csv';
 
-//    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-//    if (navigator.msSaveBlob) { // IE 10+
-//        navigator.msSaveBlob(blob, exportedFilenmae);
-//    } else {
-//        var link = document.createElement("a");
-//        if (link.download !== undefined) { // feature detection
-//            // Browsers that support HTML5 download attribute
-//            var url = URL.createObjectURL(blob);
-//            link.setAttribute("href", url);
-//            link.setAttribute("download", exportedFilenmae);
-//            link.style.visibility = 'hidden';
-//            document.body.appendChild(link);
-//            link.click();
-//            document.body.removeChild(link);
-//        }
-//    }
-//}
+    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, exportedFilenmae);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", exportedFilenmae);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
 utility.buildColoumnchart_attr = function (data, valf, Controlid,attr) {
     var arr = [];
     valf.forEach(function (v) {
